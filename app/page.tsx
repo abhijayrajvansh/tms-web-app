df51ee866d283fbd63e31ca4e3d9a115fdc5b5fe
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-08 12:11:56 +0530

    update: added shadcn/ui, components and UI config

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className="flex bred flex-1 h-screen items-center justify-center">
-      tms web app <br />
-      home screen
+    <div className="flex bred flex-1 h-screen items-center justify-center gap-3">
+      <p>
+        tms web app <br />
+        home screen
+      </p>
+      <Button>Okay!</Button>
     </div>
   );
 }
