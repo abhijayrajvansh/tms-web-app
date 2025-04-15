d2aead33659bf2c44ecb9628ab5995d98ab224bb
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-15 17:00:41 +0530

    update: added next config for origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token',
+          },
+          { key: 'Access-Control-Max-Age', value: '86400' }, // Cache preflight for 24 hours
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
