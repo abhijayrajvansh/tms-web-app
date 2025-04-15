6eab2dc2a01f46bb8b55457c13291adfe8499291
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-15 12:06:44 +0530

    update: refactor home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div className="h-screen bred flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center">
       <div className='flex items-center justify-center flex-col gap-3'>
         <p className='font-medium text-3xl'>home</p>
         <Link href={'/login'} className='text-blue-600 hover:underline'>click here to login</Link>
