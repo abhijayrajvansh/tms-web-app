2e98c237aa91f0d9271034523a10707d41cab62b
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-17 01:27:33 +0530

    update: add dashboard direct links from home

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import React from 'react';
 const page = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className='flex items-center justify-center flex-col gap-3'>
-        <p className='font-medium text-3xl'>home</p>
-        <Link href={'/login'} className='text-blue-600 hover:underline'>click here to login</Link>
+      <div className="flex items-center justify-center flex-col gap-3">
+        <p className="font-medium text-3xl">home</p>
+        <div className='flex items-center justify-center gap-3'>
+          <Link href={'/login'} className="text-blue-600 hover:underline">
+            login
+          </Link>
+          <Link href={'/dashboard'} className="text-blue-600 hover:underline">
+            dashboard
+          </Link>
+        </div>
       </div>
     </div>
   );
