f0bec88581cd153816fd6de9e70d23149d9c8fdb
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-16 19:37:22 +0530

    update: added CLIENT_URL env

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config';
+
 const env = {
   // backend
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV as string,
   SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL as string,
+  CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL as string,
 
   // app abstraction
   USERID_EMAIL: 'tms@uptut.com'
