e69fddf745eb6fa96dda6b64e2aabe45e40a1723
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-17 00:34:42 +0530

    refactor: code

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import auth from '@/auth';
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { AuthProvider } from '@/app/context/AuthContext';
 
@@ -9,10 +7,7 @@ export const dynamic = 'force-dynamic';
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   const user = await auth.getUser();
-  
-  if (!user) {
-    redirect('/login');
-  }
+  if (!user) redirect('/login')
 
   return (
     <AuthProvider user={user}>
