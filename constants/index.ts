0674942bb9e25c438b45ed704495ebfbe6e4de4c
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-04-19 16:47:05 +0530

    update: adding supabase for real time api

--- constants/index.ts
+++ constants/index.ts
@@ -5,19 +5,23 @@ const env = {
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV as string,
   SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL as string,
   // CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL as string,
-  
+
   // appwrite config & client
   ENDPOINT: process.env.NEXT_PUBLIC_ENDPOINT as string,
   PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID as string,
   API_KEY: process.env.NEXT_PUBLIC_API_KEY as string,
   DATABASE_ID: process.env.NEXT_PUBLIC_DATABASE_ID as string,
-  
-    // appwrite collections
-    COLLECTION_ORDERS: process.env.NEXT_PUBLIC_COLLECTION_ORDERS as string,
-    COLLECTION_NOTIFICATIONS: process.env.NEXT_PUBLIC_COLLECTION_NOTIFICATIONS as string,  
 
-  // appwrite app abstraction for userID email 
+  // appwrite collections
+  COLLECTION_ORDERS: process.env.NEXT_PUBLIC_COLLECTION_ORDERS as string,
+  COLLECTION_NOTIFICATIONS: process.env.NEXT_PUBLIC_COLLECTION_NOTIFICATIONS as string,
+
+  // supabase config
+  SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+
+  // appwrite app abstraction for userID email
   USERID_EMAIL: 'tms@uptut.com',
-}
+};
 
 export default env;
